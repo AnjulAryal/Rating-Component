d94102ec4f03d5ee9049be7292522a07800e368e
AnjulAryal <aryalanjul9068@gmail.com>
2025-07-18 15:11:12 +0545

    second last push

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
 const button = document.querySelector(".button");
 const boxes = document.querySelectorAll(".box");
 const ratingContainer = document.querySelector("#rating");
-console.log(ratingContainer);
+console.log(ratingContainer)
 const sucessContainer = document.querySelector("#sucess");
-console.log(sucessContainer);
+console.log(sucessContainer)
 const rating = document.querySelector(".rating");
-console.log(rating.textContent);
-boxes.forEach((box) => {
-    box.addEventListener("click", () => {
-        ratingNumber = box.getAttribute("rating-number");
+console.log(rating.textContent)
+boxes.forEach((box)=>{
+    box.addEventListener(("click"),()=>{
+        let ratingNumber = box.getAttribute("rating-number");
         rating.textContent = ratingNumber;
-        box.style.backgroundColor = "white";
-        box.style.color = "black";
-        button.addEventListener("click", () => {
-            if (rating.textContent === "") {
-                const error = document.querySelector("#error");
-                error.classList.add("active-error");
-            } else {
-                ratingContainer.classList.remove("active-container");
-                ratingContainer.classList.add("inactive-container");
-                sucessContainer.classList.add("active-container");
-                setTimeout(() => {
-                    sucessContainer.classList.remove("active-container");
-                    sucessContainer.classList.add("inactive-container");
-                    ratingContainer.classList.add("active-container");
-                    error.classList.remove("active-error");
-                }, 5000);
-            }
-        });
-    });
-});
+        box.classList.add("select");
+        button.addEventListener(("click"),()=>{
+    if(rating.textContent===""){
+        const error = document.querySelector("#error");
+        error.classList.add("active-error")
+    }else{
+        ratingContainer.classList.remove("active-container")
+        ratingContainer.classList.add("inactive-container");
+        sucessContainer.classList.add("active-container");
+        setTimeout(()=>{
+            sucessContainer.classList.remove("active-container");
+            sucessContainer.classList.add("inactive-container");
+            ratingContainer.classList.add("active-container");
+            error.classList.remove("active-error");
+            box.classList.remove("select");
+        },5000)
+    }
+
+})
+    })
+})
